516c945a89f35386d20a92eaa0979c1d3165b4aa
Elkasitu <beowolfmon_1@hotmail.com>
2017-09-02 14:59:21 +0200

    Add draw_haiku function
    This function draws a 400x400 white canvas and then writes the haiku's
    text on top of it, finally it displays it.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 
+from PIL import Image, ImageDraw, ImageFont
 from urllib.request import urlopen
 
 BASE_URL = 'http://rhymebrain.com/talk?function=getWordInfo&word=%s'
@@ -39,12 +40,21 @@ def is_haiku(haiku):
     for sentence in sentences:
         actual.append(get_line_syllables(sentence))
 
+    print(actual)
+
     return actual == correct
+
+def draw_haiku(haiku):
+    im = Image.new("RGBA", (400, 400), (255, 255, 255))
+    dr = ImageDraw.Draw(im)
+    font = ImageFont.truetype("~/fonts/Hack/Hack-Regular.ttf", 35)
+    text_size = dr.textsize(haiku, font)
+    dr.text(
+        ((im.size[0] - text_size[0]) / 2, (im.size[1] - text_size[1]) / 2),
+        haiku, (0, 0, 0), font=font
+    )
+    im.show()
 
 
 if __name__ == '__main__':
-    haiku = """Forget the sadness
-    You will not be alone
-    Honeydew melon
-    """
-    print(is_haiku(haiku))
+    draw_haiku("Hello World!\nHello World!\nHelloWorld!")
